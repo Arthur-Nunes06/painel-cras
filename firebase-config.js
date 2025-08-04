0a7da1dd935ad9a9e6f17949d23dd229050b313c
Arthur-Nunes06 <138708548+Arthur-Nunes06@users.noreply.github.com>
2025-08-03 22:22:41 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,8 @@
 // firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-database.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyChT_LbedW9M1lsyK26HUCCDCcJc4hoQf4",
   authDomain: "painelcras-4890c.firebaseapp.com",
@@ -9,5 +13,10 @@ const firebaseConfig = {
   appId: "1:279841059682:web:be689ab69135f04cb4323b"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+signInAnonymously(auth).catch(console.error);
+
+export { db, auth };
